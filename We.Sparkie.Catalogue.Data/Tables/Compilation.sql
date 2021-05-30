348ae1121933a478b57e674f64336ac4852e15c1
jolySoft <jolyon.wharton@webenabled.tv>
2021-05-30 19:31:14 +0100

    removed migrations because they're shit, renamed WHOLE service to catalogue, add datpak (not working yet)

---
+++ We.Sparkie.Catalogue.Data/Tables/Compilation.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Compilation]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[CompilationType] INT NOT NULL,
+	[CreatedBy] NVARCHAR(256) NOT  NULL
+)
---
+++ We.Sparkie.Catalogue.Data/Tables/Track.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Track]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[DigitalAssetId] UNIQUEIDENTIFIER NOT NULL,
+	[Name] NVARCHAR(1024),
+	[CompilationType] INT,
+	[Artist_Id]  UNIQUEIDENTIFIER NOT NULL,
+	[Compilation_Id] UNIQUEIDENTIFIER NOT NULL,
+	CONSTRAINT Track_Artist_Id_Fk FOREIGN KEY (Artist_Id) REFERENCES Artist(Id),
+	CONSTRAINT Track_Compilation_Id FOREIGN KEY (Compilation_Id) REFERENCES Compilation(Id)
+)
